6d8c4312775cd9642f8dffe204f24e273e43966f
ZhenfeiYu <55257370+ZhenfeiYu@users.noreply.github.com>
2020-02-22 20:42:40 -0500

    Add files via upload

--- test_twitter.py
+++ test_twitter.py
@@ -2,7 +2,7 @@ from twitter import get_tweets
 def test_get_tweets():
     assert get_tweets('blakelively','blakelively')
     assert get_tweets('EXO','EXO')
-    assert get_tweets('blakelively','blakelively')
+    assert get_tweets('sehun','sehun')
     assert get_tweets('baekhyun','baekhyun')
     assert get_tweets('evanlin','evanlin')
     assert get_tweets('chanyeol','chanyeol')
